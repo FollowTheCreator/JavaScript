32b6ac6f0b9ce5085ab03ce70ff7e113ac6592db
Artyom <Artyomkovalevich.top@gmail.com>
2019-10-03 12:20:43 +0300

    Refactor ArrayLibUsage

--- FunctionalArrayLibrary/Assets/JS/ArrayLibUsage.js
+++ FunctionalArrayLibrary/Assets/JS/ArrayLibUsage.js
@@ -5,5 +5,5 @@ console.log(ArrayLib.reduce([1, 2, 3, 4, 5], (sum, current) => sum + current, 0)
 console.log(ArrayLib.filter([1, 2, 3, 4, 5], item => item > 3));
 ArrayLib.foreach([1, 2, 3], item => console.log(item));
 
-console.log(ArrayLib.chain([1, 2, 3, 4, 5, 6, 7, 8, 9]).take(8).skip(1).map(a => a * 2).filter(item => item > 5).reduce((sum, current) => sum + current, 0).value());
+console.log(ArrayLib.chain([1, 2, 3, 4, 5, 6, 7, 8, 9]).take(8).skip(1).map(a => a * 2).filter(item => item > 5).reduce((sum, current) => sum + current, 0));
 ArrayLib.chain([1, 2, 3, 4, 5, 6, 7, 8, 9]).take(8).skip(1).map(a => a * 2).filter(item => item > 5).foreach(item => console.log(item));
