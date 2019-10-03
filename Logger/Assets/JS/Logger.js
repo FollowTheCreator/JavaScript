1b472be84fc83c0e50258b58e785f2a2990857f7
Artyom <Artyomkovalevich.top@gmail.com>
2019-10-03 12:29:08 +0300

    Create another log function

--- Logger/Assets/JS/Logger.js
+++ Logger/Assets/JS/Logger.js
@@ -2,4 +2,9 @@ const log = (message) => {
     console.log(message);
 }
 
-log("Hellow World!");
+const anotherLog = (...args) => {
+    const message = args.join(" | ");
+    log(message);
+}
+
+anotherLog("Hello", "World", "!");
